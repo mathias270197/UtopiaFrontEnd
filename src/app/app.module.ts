a03006f08618da6ef7eebf2f75ab0f96367b43ff
Stijn De Preter <stijndepreter@live.be>
2022-11-30 22:05:48 +0100

    login toegevoegd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './login/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  userName: string = '';
+  errorMessage: string = '';
+
+  constructor(private router: Router, private loginService: LoginService) { }
 
   ngOnInit(): void {
   }
+
+  ngOnDestroy(): void {
+  }
+
+  onSubmit() {
+    //validatie toevoegen voor login name: mag niet "currentUserName" of "personalKey" zijn!!!
+    this.loginService.login(this.userName)
+    //this.router.navigateByUrl("/game")
+  }
+
+
 
 }

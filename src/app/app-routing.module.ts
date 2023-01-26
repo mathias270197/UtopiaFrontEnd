e75c807336dbd91649f3b298672689c91679a9ca
johnny urkens <58312023+johnny-urkens@users.noreply.github.com>
2023-01-26 10:57:32 +0100

    cleanup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login/login.component';
 import { RouterModule, Routes } from '@angular/router';
 import { StationsComponent } from './rail-map/stations/stations.component';
-import { NeighbourhoodComponent } from './rail-map/neighbourhood/neighbourhood.component';
+
 import { FormTemplateComponent } from './form-template/form-template/form-template.component';
 import { MetrolineComponent } from './metroline/metroline/metroline.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'stations', component: StationsComponent },
-  { path: 'neighbourhood', component: NeighbourhoodComponent },
+
   { path: 'form', component: FormTemplateComponent},
   { path: 'line', component: MetrolineComponent}
 ];
